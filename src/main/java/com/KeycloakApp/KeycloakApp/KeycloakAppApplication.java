fceb2cdec2855bb427c334d7ef69c4478018e7f7
AzizKale <aziz.kale@emm-it.de>
2024-01-25 13:07:57 +0100

    initial commit

---
+++ src/main/java/com/KeycloakApp/KeycloakApp/KeycloakAppApplication.java
@@ -0,0 +1,13 @@
+package com.KeycloakApp.KeycloakApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KeycloakApp/KeycloakApp/KeycloakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KeycloakApp.KeycloakApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
